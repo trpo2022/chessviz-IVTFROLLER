450634b78d265edb246a81d99d5b227a2003adab
Froller <andrey.pipisyn@gmail.com>
2022-03-29 16:49:15 +0700

    Initialize 'scr' folder

---
+++ src/chess/main.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include "printBoard.h"
+
+#define BOARD_ROWS 10
+#define BOARD_COLUMNS 10
+
+
+
+int main()
+{
+    int flag = 1;
+    char move[6];
+
+
+    char board[BOARD_ROWS][BOARD_COLUMNS]
+            = {{'8', '|', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+               {'7', '|', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
+               {'6', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {'5', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {'4', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {'3', '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
+               {'2', '|', 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
+               {'1', '|', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'},
+               {' ', ' ', '_', '_', '_', '_', '_', '_', '_', '_'},
+               {' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
+               };
+
+    while(flag != 0){
+        linePrint();
+        printf("\n");
+        boardPrint(board);
+        printf("\nMove: ");
+        for(int i = 0; i < 6;i++){
+            scanf("%c", &move[i]);
+            if(move[0] == 'q'){
+                flag = 0;
+                break;
+            }
+        }
+
+        printf("\nThe move was: %s\n", move);
+        
+    }
+
+    printf("\n");
+
+    return 0;
+}
---
+++ src/libchess/printBoard.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#define BOARD_ROWS 10
+#define BOARD_COLUMNS 10
+
+void boardPrint(char board[BOARD_ROWS][BOARD_COLUMNS]);
+
+void linePrint();
