83ab0dbb36c1475c1ce50da3491aace1372d5086
Froller <andrey.pipisyn@gmail.com>
2022-03-29 16:37:05 +0700

    Initialize the main body of the function for movement

--- main.c
+++ main.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
+#include "printBoard.h"
 
 #define BOARD_ROWS 10
 #define BOARD_COLUMNS 10
 
 
-void boardPrint(char board[BOARD_ROWS][BOARD_COLUMNS])
-{
-    for (int i = 0; i < BOARD_COLUMNS; i++) {
-        for (int j = 0; j < BOARD_ROWS; j++) {
-            printf(" %c", board[i][j]);
-        }
-        printf("\n");
-    }
-
-}
 
 int main()
 {
+    int flag = 1;
+    char move[6];
+
+
     char board[BOARD_ROWS][BOARD_COLUMNS]
             = {{'8', '|', 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
                {'7', '|', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
@@ -30,8 +25,24 @@ int main()
                {' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'},
                };
 
+    while(flag != 0){
+        linePrint();
+        printf("\n");
+        boardPrint(board);
+        printf("\nMove: ");
+        for(int i = 0; i < 6;i++){
+            scanf("%c", &move[i]);
+            if(move[0] == 'q'){
+                flag = 0;
+                break;
+            }
+        }
+
+        printf("\nThe move was: %s\n", move);
+        
+    }
+
     printf("\n");
-    boardPrint(board);
 
     return 0;
 }
