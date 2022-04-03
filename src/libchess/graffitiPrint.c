d9dd3f8a9a2a7562aaa2450d23ceb08d5ce9d9e6
Froller <andrey.pipisyn@gmail.com>
2022-04-03 22:45:32 +0700

    Create dedicated subheader file for graffiti priting

---
+++ src/libchess/graffitiPrint.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void graffitiPrint()
+{
+    printf("\n");
+    printf("            d8b                                  d8b                       ,d8888b                  d8b  d8b                 ");
+    printf("            ?88                                  ?88                       88P'                     88P  88P                 ");
+    printf("             88b                                  88b                   d888888P                   d88  d88                  ");
+    printf("     d8888b  888888b  d8888b .d888b, .d888b,      888888b ?88   d8P       ?88'      88bd88b d8888b 888  888   d8888b  88bd88b");
+    printf("    d8P' `P  88P `?8bd8b_,dP ?8b,    ?8b,         88P `?8bd88   88        88P       88P'  `d8P' ?88?88  ?88  d8b_,dP  88P'  `");
+    printf("    88b     d88   88P88b       `?8b    `?8b      d88,  d88?8(  d88       d88       d88     88b  d88 88b  88b 88b     d88     ");
+    printf("    `?888P'd88'   88b`?888P'`?888P' `?888P'     d88'`?88P'`?88P'?8b     d88'      d88'     `?8888P'  88b  88b`?888P'd88'     ");
+    printf("                                                                 )88                                                         ");
+    printf("                                                                ,d8P                                                         ");
+    printf("                                                             `?888P'                                                         ");
+    printf("\n");
+
+}
+
+
+/***
+ *            d8b                                  d8b                       ,d8888b                  d8b  d8b                 
+ *            ?88                                  ?88                       88P'                     88P  88P                 
+ *             88b                                  88b                   d888888P                   d88  d88                  
+ *     d8888b  888888b  d8888b .d888b, .d888b,      888888b ?88   d8P       ?88'      88bd88b d8888b 888  888   d8888b  88bd88b
+ *    d8P' `P  88P `?8bd8b_,dP ?8b,    ?8b,         88P `?8bd88   88        88P       88P'  `d8P' ?88?88  ?88  d8b_,dP  88P'  `
+ *    88b     d88   88P88b       `?8b    `?8b      d88,  d88?8(  d88       d88       d88     88b  d88 88b  88b 88b     d88     
+ *    `?888P'd88'   88b`?888P'`?888P' `?888P'     d88'`?88P'`?88P'?8b     d88'      d88'     `?8888P'  88b  88b`?888P'd88'     
+ *                                                                 )88                                                         
+ *                                                                ,d8P                                                         
+ *                                                             `?888P'                                                         
+ */
